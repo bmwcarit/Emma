d4adafa8f9bd75a425a34e9d92006283cc6e79fe
Kocsis András Gergő <kocsisandrasgergo@gmail.com>
2019-08-01 14:23:35 +0200

    Reworking the test__cmd_line.py
    Details:
    	- The new design copies the test_project for every test
    	- Added the TestHelper class for creating and cleaning up an environment for the tests
    	- Added new tests
    	- Bugfixes

--- tests/functional_tests/test__cmd_line.py
+++ tests/functional_tests/test__cmd_line.py
@@ -24,121 +24,221 @@ import sys
 import os
 import shutil
 
-sys.path.append("../")
+from pypiscout.SCout_Logger import Logger as sc
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 # pylint: disable=wrong-import-position
 # Rationale: This module needs to access modules that are above them in the folder structure.
 
 import emma
 import emma_vis
 
-import shared_libs.emma_helper
+
+class TestHelper(unittest.TestCase):
+    """
+    Helper class for tests that creates an environment basic setup.
+    This includes making a copy of the test_project so we can work on a separate copy and not modify the original one.
+    This also ensures that all the tests can work on a clean test_project state.
+    """
+    def init(self, testCaseName):
+        # Setting up the logger
+        # This syntax will default init it and then change the settings with the __call__()
+        # This is needed so that the unit tests can have different settings and not interfere with each other
+        sc()(4, actionWarning=None, actionError=TestHelper.actionError)
+
+        # Switching to the Emma root folder
+        os.chdir(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+        # Defining the paths of the folders used during the tests
+        self.cmdLineTestRootFolder = os.path.join("tests", "other_files", "test__cmd_line")
+        # Defining a path that shall contain the project files
+        self.cmdLineTestProjectFolder = os.path.join(self.cmdLineTestRootFolder, "test_project")
+        # Defining a path that shall contain the mapfiles
+        self.cmdLineTestProjectMapfilesfolder = os.path.join(self.cmdLineTestProjectFolder, "mapfiles")
+        # Defining a path that shall contain the output
+        self.cmdLineTestOutputFolder = os.path.join("tests", "other_files", "test__cmd_line", testCaseName)
+        # Defining a path that shall not exist
+        self.nonExistingPath = os.path.join(self.cmdLineTestRootFolder, "this", "directory", "does", "not", "exist")
+
+        # Checking whether the root folder still exist from the previous run, if it does, we shall not erase it, but ask the user to do it manually
+        self.assertFalse(os.path.isdir(self.cmdLineTestRootFolder), "The temporary folder (\"" + self.cmdLineTestRootFolder + "\") still exists! Please delete it manually.")
+
+        # Defining the location of the source test_project
+        sourceTestProjectFolder = os.path.join("doc", "test_project")
+
+        # Creating the root folder
+        os.makedirs(self.cmdLineTestProjectFolder)
+        # Copying the project files
+        for file in os.listdir(sourceTestProjectFolder):
+            if os.path.splitext(file)[-1].lower() == ".json":
+                shutil.copy(os.path.join(sourceTestProjectFolder, file), self.cmdLineTestProjectFolder)
+        # Copying the mapfiles
+        shutil.copytree(os.path.join(sourceTestProjectFolder, "mapfiles"), os.path.join(self.cmdLineTestProjectFolder, "mapfiles"))
+        # Creating the output folder for the results with the test case name
+        os.makedirs(self.cmdLineTestOutputFolder)
+
+    @staticmethod
+    def actionError():
+        sys.exit(-10)
+
+    def deInit(self):
+        # Checking whether the non existing Path exists. If it does then it was created by the software, which is an error. We will delete it so it does not influence the other tests.
+        nonExistingPathErrorDetected = os.path.isdir(self.nonExistingPath)
+        # Deleting the output folder of this test case
+        shutil.rmtree(self.cmdLineTestRootFolder)
+        self.assertFalse(nonExistingPathErrorDetected, "The non-existing path (\"" + self.nonExistingPath + "\") exists at tearDown! This path shall never be created by the software. To avoid effects on other tests, it was now deleted.")
 
 
-class CmdEmma(unittest.TestCase):
+class CmdEmma(TestHelper):
     # pylint: disable=invalid-name
     # Rationale: Tests need to have the following method names in order to be discovered: test_<METHOD_NAME>().
 
+    """
+    Class containing tests for testing the command line argument processing for Emma.
+    """
+    def setUp(self):
+        self.init("CmdEmma")
+
+    def tearDown(self):
+        self.deInit()
+
+    def test_normalRun(self):
+        """
+        Check that an ordinary run is successful
+        """
+        try:
+            args = emma.parseArgs(["--project", self.cmdLineTestProjectFolder, "--mapfiles", self.cmdLineTestProjectMapfilesfolder, "--dir", self.cmdLineTestOutputFolder])
+            emma.main(args)
+        except Exception as e:
+            self.fail("Unexpected exception: " + str(e))
+
     def test_help(self):
         """
-        Check that `--help` does not raise an exeption
+        Check that `--help` does not raise an exeption but exits with SystemExit(0)
         """
-        # with self.assertRaises(SystemExit):
-        try:
+        with self.assertRaises(SystemExit) as context:
             args = emma.parseArgs(["--help"])
             emma.main(args)
-        except SystemExit as e:
-            if e.code != 0:
-                print("Exit code", e.code)
-                raise e
+        self.assertEqual(context.exception.code, 0)
 
     def test_unrecognisedArgs(self):
         """
-        Check that `--help` does not raise an exeption
+        Check that an unexpected argument does raise an exeption
         """
-        # with self.assertRaises(SystemExit):
+        with self.assertRaises(SystemExit) as context:
+            args = emma.parseArgs(["--project", self.cmdLineTestProjectFolder, "--mapfiles", self.cmdLineTestProjectMapfilesfolder, "--dir", self.cmdLineTestOutputFolder, "--blahhhhhh"])
+            emma.main(args)
+        self.assertEqual(context.exception.code, 2)
+
+    def test_noProjDir(self):
+        """
+        Check run with non-existing project folder
+        """
+        with self.assertRaises(SystemExit) as context:
+            args = emma.parseArgs(["--project", self.nonExistingPath, "--mapfiles", self.cmdLineTestProjectMapfilesfolder, "--dir", self.cmdLineTestOutputFolder])
+            emma.main(args)
+        self.assertEqual(context.exception.code, -10)
+
+    def test_noMapfileDir(self):
+        """
+        Check run with non-existing mapfile folder
+        """
+        with self.assertRaises(SystemExit) as context:
+            args = emma.parseArgs(["--project",  self.cmdLineTestProjectFolder, "--mapfiles", self.nonExistingPath, "--dir", self.cmdLineTestOutputFolder])
+            emma.main(args)
+        self.assertEqual(context.exception.code, -10)
+
+    def test_noDirOption(self):
+        """
+        Check run without a --dir parameter
+        """
         try:
-            args = emma.parseArgs(["--project", "doc/test_project", "--mapfiles", "doc/test_project/mapfiles", "--blahhhhhh"])
+            args = emma.parseArgs(["--project", self.cmdLineTestProjectFolder, "--mapfiles", self.cmdLineTestProjectMapfilesfolder])
             emma.main(args)
-        except SystemExit as e:
-            if e.code != 2:
-                print("Exit code", e.code)
-                raise e
-
-    def test_noProjMapfileDir(self):
-        with self.assertRaises(SystemExit):
-            args = emma.parseArgs(["--project", "this/directory/does/not/exist", "--mapfiles", "this/directory/does/not/exist"])
-            emma.main(args)
-
-    @staticmethod
-    def test_normalRun():
-        """
-        Check that an ordinary run is successful
-        """
-        os.chdir("../")
-        args = emma.parseArgs(["--project", "doc/test_project", "--mapfiles", "doc/test_project/mapfiles"])
-        emma.main(args)
-        os.chdir("tests")
+        except Exception as e:
+            self.fail("Unexpected exception: " + str(e))
 
 
-class CmdEmmaVis(unittest.TestCase):
+class CmdEmmaVis(TestHelper):
     # pylint: disable=invalid-name
     # Rationale: Tests need to have the following method names in order to be discovered: test_<METHOD_NAME>().
 
-    def test_help(self):
-        """
-        Check that `--help` does not raise an exeption
-        """
-        # with self.assertRaises(SystemExit):
-        try:
-            args = emma_vis.parseArgs(["--help"])
-            emma_vis.main(args)
-        except SystemExit as e:
-            if e.code != 0:
-                print("Exit code", e.code)
-                raise e
+    """
+    Class containing tests for testing the command line argument processing for Emma-Vis.
+    """
 
-    @staticmethod
-    def test_normalRun():
+    def setUp(self):
+        self.init("CmdEmmaVis")
+        self.runEmma(self.cmdLineTestOutputFolder)
+
+    def tearDown(self):
+        self.deInit()
+
+    def runEmma(self, outputFolder=None):
+        if outputFolder is not None:
+            args = emma.parseArgs(["--project", self.cmdLineTestProjectFolder, "--mapfiles", self.cmdLineTestProjectMapfilesfolder, "--dir", outputFolder, "--noprompt"])
+        else:
+            args = emma.parseArgs(["--project", self.cmdLineTestProjectFolder, "--mapfiles", self.cmdLineTestProjectMapfilesfolder, "--noprompt"])
+        emma.main(args)
+
+    def test_normalRun(self):
         """
         Check that an ordinary run is successful
         """
-        os.chdir("../")
-        # args = emma.parseArgs(["--project", "doc/test_project", "--mapfiles", "doc/test_project/mapfiles"])
-        # emma.main(args)
-
-        args = emma_vis.parseArgs(["--project", "doc/test_project", "--overview", "--quiet"])
-        emma_vis.main(args)
-        os.chdir("tests")
-
-    @staticmethod
-    def test_runWithNoMemStats():
-        """
-        No memStats should not raise an exception but exit with sys.exit(-10)
-        """
-        os.chdir("../")
-        tempMemStatsPath = "temp"
-        tempMemStatsPathMemStats = shared_libs.emma_helper.joinPath(tempMemStatsPath, "memStats")
-        if os.path.isdir(tempMemStatsPathMemStats):
-            os.removedirs(tempMemStatsPathMemStats)
-        os.makedirs(tempMemStatsPathMemStats)
-
-        # Make sure the directory is empty
-        assert len(os.listdir(tempMemStatsPathMemStats)) == 0
-
-        def cleanUp():
-            shutil.rmtree(tempMemStatsPath)
-            os.chdir("tests")
         try:
-            args = emma_vis.parseArgs(["--project", "doc/test_project", "--overview", "--dir", tempMemStatsPath])
+            args = emma_vis.parseArgs(["--project", self.cmdLineTestProjectFolder, "--overview", "--dir", self.cmdLineTestOutputFolder, "--noprompt", "--quiet"])
             emma_vis.main(args)
-        except SystemExit as e:
-            if -10 != e.code:
-                cleanUp()
-                print("Exit code", e.code)
-                raise e
-            else:
-                # All good; program ends with sys.exit(-10) since no file was found
-                cleanUp()
+        except Exception as e:
+            self.fail("Unexpected exception: " + str(e))
+
+    def test_help(self):
+        """
+        Check that `--help` does not raise an exeption but exits with SystemExit(0)
+        """
+        with self.assertRaises(SystemExit) as context:
+            args = emma_vis.parseArgs(["--help"])
+            emma_vis.main(args)
+        self.assertEqual(context.exception.code, 0)
+
+    def test_unrecognisedArgs(self):
+        """
+        Check that an unexpected argument does raise an exeption
+        """
+        with self.assertRaises(SystemExit) as context:
+            args = emma_vis.parseArgs(["--project", self.cmdLineTestProjectFolder, "overview", "--dir", self.cmdLineTestOutputFolder, "--blahhhhhh", "--noprompt", "--quiet"])
+            emma_vis.main(args)
+        self.assertEqual(context.exception.code, 2)
+
+    def test_noProjDir(self):
+        """
+        Check run with non-existing project folder
+        """
+        with self.assertRaises(SystemExit) as context:
+            args = emma_vis.parseArgs(["--project", self.nonExistingPath, "--overview", "--dir", self.cmdLineTestOutputFolder, "--noprompt", "--quiet"])
+            emma_vis.main(args)
+        self.assertEqual(context.exception.code, -10)
+
+    def test_noMemStats(self):
+        """
+        Check run with non-existing memStats folder
+        """
+        with self.assertRaises(SystemExit) as context:
+            args = emma_vis.parseArgs(["--project", self.cmdLineTestProjectFolder, "--overview", "--dir", self.nonExistingPath, "--noprompt", "--quiet"])
+            emma_vis.main(args)
+        self.assertEqual(context.exception.code, -10)
+
+    def test_noDirOption(self):
+        """
+        Check run without a --dir parameter
+        """
+        try:
+            # This a is a specific case, the default Emma results will not work here. Because of this, we will delete it and run the Emma again.
+            shutil.rmtree(self.cmdLineTestOutputFolder)
+            self.runEmma()
+            args = emma_vis.parseArgs(["--project", self.cmdLineTestProjectFolder, "--overview", "--noprompt", "--quiet"])
+            emma_vis.main(args)
+        except Exception as e:
+            self.fail("Unexpected exception: " + str(e))
 
 
 if __name__ == '__main__':
