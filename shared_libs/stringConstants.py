4ae4c3e84b0f93c90137914780a24fcd43ed1883
Marcel Schmalzl <holzkohlengrill@gmail.com>
2019-08-05 18:14:02 +0200

    Version bump to 3.0 - memory manager redesign; #5

--- shared_libs/stringConstants.py
+++ shared_libs/stringConstants.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 
 # Version ################################################
-VERSION_MAJOR = "2"
+VERSION_MAJOR = "3"
 VERSION_MINOR = "0"
 EMMA_VERSION = ".".join([VERSION_MAJOR, VERSION_MINOR])
 EMMA_VISUALISER_VERSION = EMMA_VERSION
