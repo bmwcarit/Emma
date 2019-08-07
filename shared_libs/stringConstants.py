d220266cf1ec73f70824c211ff7fbbef2dd53465
Marcel Schmalzl <holzkohlengrill@gmail.com>
2019-08-07 17:24:54 +0200

    Version bump to 2.1

--- shared_libs/stringConstants.py
+++ shared_libs/stringConstants.py
@@ -21,7 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 # Version ################################################
 VERSION_MAJOR = "2"
-VERSION_MINOR = "0"
+VERSION_MINOR = "1"
 EMMA_VERSION = ".".join([VERSION_MAJOR, VERSION_MINOR])
 EMMA_VISUALISER_VERSION = EMMA_VERSION
 EMMA_DELTAS_VERSION = EMMA_VERSION
