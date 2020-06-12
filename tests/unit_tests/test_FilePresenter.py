9203d2427eed81c793e6baa9228e5cf471a5ef78
Daria Pigasova <dariapigasova@gmail.com>
2020-06-12 22:12:50 +0200

    bug fixes

--- tests/unit_tests/test_FilePresenter.py
+++ tests/unit_tests/test_FilePresenter.py
@@ -11,13 +11,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 class FilePresenterTestCase(TestCase):
 
     def setUp(self) -> None:
-        newFolder = os.path.join("..", "..")
-        projectDir = os.path.join(newFolder, "testcase")
-        testFile1 = os.path.join(projectDir, "memStats", "Object_Summary_2020_05_06.csv")
-        testFile2 = os.path.join(projectDir, "memStats", "Object_Summary_2020_04_06.csv")
-        self.FileSelector = Emma.emma_delta_libs.FileSelector.FileSelector(projectDir)
+        self.thisDir = os.path.dirname(os.path.abspath(__file__))
+
+        self.testFile1 = os.path.join(self.thisDir, "memStats", "Object_Summary_2020_05_06.csv")
+        self.testFile2 = os.path.join(self.thisDir, "memStats", "Object_Summary_2020_04_06.csv")
+        self.FileSelector = Emma.emma_delta_libs.FileSelector.FileSelector(self.thisDir)
         self.filePresenter = Emma.emma_delta_libs.FilePresenter.FilePresenter(self.FileSelector)
-        self.candidates = {1: testFile1, 2: testFile2}
+        self.candidates = {1: self.testFile1, 2: self.testFile2}
 
     def test_selectNumber(self):
         self.assertEqual(False, Emma.emma_delta_libs.FilePresenter.FilePresenter.validateNumber(self.filePresenter, "1 4"))
