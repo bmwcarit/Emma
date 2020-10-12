83ca2eeee780a6454ee4cc4d04d554a7283382e3
DariaPigasova <53271013+DariaPigasova@users.noreply.github.com>
2020-10-12 13:10:11 +0200

    close #6, added dry run argument (#70)
    * close #6, added an argument for allowing not to store any standard reports
    
    * close #6, added an argument for allowing not to store any standard reports
    
    * removed unnecessary function argument
    
    Co-authored-by: Marcel Schmalzl <12732886+holzkohlengrill@users.noreply.github.com>

--- Emma/emma_libs/memoryManager.py
+++ Emma/emma_libs/memoryManager.py
@@ -41,7 +41,7 @@ class MemoryManager:
         """
         Settings that influence the operation of the MemoryManager object.
         """
-        def __init__(self, projectName, configurationPath, mapfilesPath, outputPath, analyseDebug, createCategories, removeUnmatched, noPrompt, noResolveOverlap, teamScale):
+        def __init__(self, projectName, configurationPath, mapfilesPath, outputPath, analyseDebug, createCategories, removeUnmatched, noPrompt, noResolveOverlap, teamScale, dryRun):
             self.projectName = projectName
             self.configurationPath = configurationPath
             self.mapfilesPath = mapfilesPath
@@ -52,13 +52,14 @@ class MemoryManager:
             self.noPrompt = noPrompt
             self.noResolveOverlap = noResolveOverlap
             self.teamScale = teamScale
+            self.dryRun = dryRun
 
-    def __init__(self, projectName, configurationPath, mapfilesPath, outputPath, analyseDebug, createCategories, removeUnmatched, noPrompt, noResolveOverlap, teamScale):
+    def __init__(self, projectName, configurationPath, mapfilesPath, outputPath, analyseDebug, createCategories, removeUnmatched, noPrompt, noResolveOverlap, teamScale, dryRun):
         # pylint: disable=too-many-arguments
         # Rationale: We need to initialize the Settings, so the number of arguments are needed.
 
         # Processing the command line arguments and storing it into the settings member
-        self.settings = MemoryManager.Settings(projectName, configurationPath, mapfilesPath, outputPath, analyseDebug, createCategories, removeUnmatched, noPrompt, noResolveOverlap, teamScale)
+        self.settings = MemoryManager.Settings(projectName, configurationPath, mapfilesPath, outputPath, analyseDebug, createCategories, removeUnmatched, noPrompt, noResolveOverlap, teamScale, dryRun)
         # Check whether the configuration and the mapfiles folders exist
         Emma.shared_libs.emma_helper.checkIfFolderExists(self.settings.mapfilesPath)
         self.configuration = None                   # The configuration is empty at this moment, it can be read in with another method
@@ -213,8 +214,9 @@ class MemoryManager:
             Emma.shared_libs.emma_helper.writeJson(reportPath, resultsLst)
 
         if self.memoryContent is not None:
-            # TODO: Implement handling and choosing of which reports to create (via cmd line argument (like a comma separted string) (MSc)
+            # TODO: Implement handling and choosing of which reports to create (via cmd line argument (like a comma separated string) (MSc)
             createStandardReports()
+
             # createDotReports()
             if teamscale:
                 createTeamScaleReports()
